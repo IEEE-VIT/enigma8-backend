8f2f69dfc1581fed4c679d3e67f2b6396450e4d5
varunmurpani <varunmurpani10@gmail.com>
2021-09-28 05:25:20 +0530

    created room schema

---
+++ models/roomModel.js
@@ -0,0 +1,42 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const User= require('./userModel')
+const Room= require('./roomModel')
+const Powerup= require('./powerupModel')
+const Question= require('./questionModel')
+require('mongoose-type-url');
+
+const roomSchema= new mongoose.Schema({
+
+  roomId: {
+    type: String
+  },
+
+  roomNo: {
+    type: String
+  },
+  
+  
+  questionId: [{
+    type: mongoose.Schema.Types.String,     //not sure about this part.
+    ref: 'Question'
+  }],
+
+  media: {
+    type: mongoose.SchemaTypes.Url
+  },
+
+  title: {
+    type: String
+  },
+
+  starQuota: {
+    type: Number
+  }
+
+}
+);
+
+const Room = mongoose.model('Room', roomSchema);
+
+module.exports = Room;
