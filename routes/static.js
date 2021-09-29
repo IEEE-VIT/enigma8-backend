041fd21b4e11052e30d53ea6b4558cecc3ee130a
Sidarth Venkatesan <sidarth.v14002@gmail.com>
2021-09-29 18:02:55 +0530

    static routes intro and rules added

--- routes/static.js
+++ routes/static.js
@@ -8,4 +8,20 @@ router.get("/timer", async (req, res) => {
   response(res, getTimer());
 });
 
+var intro = "This is a sample intro"
+
+router.get("/intro", async (req, res) => {
+  response(res, {
+    intro: intro,
+  });
+});
+
+var rules = ["Rule 1", "Rule 2", "Rule 3"]
+
+router.get("/rules", async (req, res) => {
+  response(res, {
+    rules: rules,
+  });
+});
+
 module.exports = router;
