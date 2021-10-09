fdc554f00565776813eda7aed9a318097e0ece17
Sidarth Venkatesan <sidarth.v14002@gmail.com>
2021-10-09 17:06:33 +0530

    array of rooms
    rooms rebase with master

--- controllers/roomController.js
+++ controllers/roomController.js
@@ -1,15 +1,28 @@
 const Room = require("../models/roomModel");
-const jou = require("../models/journeyModel");
-const { createUserSchema } = require("../config/requestSchema");
+const Journey = require("../models/journeyModel");
 const { response } = require("../config/responseSchema");
+const { createUserSchema } = require("../config/requestSchema");
 
 const getRooms = async (req, res) => {
     try{
-        const userid = req.Journey.userId;
+        const userId = req.user.email;
+        const userRoms = await Journey.find({userId});
+        const rooms = await Room.find({});
+        const userRooms = new Set(userRoms.roomId);
+       
+        const data = [];
+        rooms.forEach(item => {
+            if(userRooms.has(item.roomId)){
+                let jou = userRoms.find(a => a.roomId == item.RoomId)
+                data.push([item.roomId, item.roomNo, item.media, item.title, item.star_quota, jou.roomUnlocked, jou.power_up_id, jou.power_up_id, jou.stars]);
+            }
+        });
+
+        response(res, { rooms : data});
         
     }catch(err){
         response(res, {}, 400, err.message, false);
     }
 };
 
-module.exports = getRooms;
+module.exports = {getRooms};
