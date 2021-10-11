e4dec9f5534a91864ad55ac5274bd7bf9ad21777
anshul_ubuntu <ans29hul@gmail.com>
2021-10-11 16:01:33 +0530

    select powerup

--- config/requestSchema.js
+++ config/requestSchema.js
@@ -1,4 +1,5 @@
 const Joi = require("joi");
+Joi.objectId = require("joi-objectid")(Joi);
 exports.createUserSchema = Joi.object({
   username: Joi.string().alphanum().min(3).max(30).required(),
   isCollegeStudent: Joi.boolean().required(),
@@ -6,4 +7,8 @@ exports.createUserSchema = Joi.object({
     .email({ tlds: { allow: false } })
     .required(),
   outreach: Joi.string(),
+});
+exports.consumePowerupSchema = Joi.object({
+  roomId: Joi.objectId().required(),
+  powerupId: Joi.objectId().required(),
 });
