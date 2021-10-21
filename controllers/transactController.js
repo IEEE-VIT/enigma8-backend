673320c7646b2404f94414f4bdf4c90a37c0d4aa
varunmurpani <varunmurpani10@gmail.com>
2021-10-21 17:58:23 +0530

    submit answer and update score, stars and question status

--- controllers/transactController.js
+++ controllers/transactController.js
@@ -42,3 +42,81 @@ exports.getQuestion = async(req,res) => {
   }
   
 }
+
+exports.submitAnswer= async(req,res) =>{
+  try {
+    const userId= req.user.id;
+    const userAnswer= req.body.userAnswer;
+    const userAnswerLower= userAnswer.toLowerCase();
+    const roomId= req.body.roomId;
+    const currentJourney= await Journey.findOne({roomId,userId})
+
+    let i=0;
+    let answerState=0;
+    for(i=0; i<3;i++)
+    {
+      if(currentJourney.questionsStatus[i] === "unlocked")
+      {    
+        const currentRoom= await Room.findOne({_id: roomId})           
+        const questionId= currentRoom.questionId[i]      
+        const currentQuestion= await Question.findOne({_id: questionId})
+
+        
+        // 0= incorrect ans    1= close answer    2= correct answer
+
+        const correctAnswers= new Set(currentQuestion.answers)
+        const closeAnswers= new Set(currentQuestion.closeAnswers)
+        if(correctAnswers.has(userAnswerLower)) 
+        {
+          session.startTransaction();
+          //update score using dynamic algo      
+          const addStarAndScore= await User.updateOne(
+            {userId},
+            {$inc:{stars:1}, $inc: {score:50}}, 
+            {session});
+
+          if(!addStarAndScore) throw new Error("Error updating stars and score")
+          answerState= 2;
+
+          const updateStatus= Journey.updateOne(
+            {userId,roomId},
+            {$set: {questionsStatus[i]: "solved"}},
+            {session});
+          if(!updateStatus) throw new Error("Error updating status of question")
+          
+          //change locked to unlocked next question
+      
+          //if stars enough to unlock the next room(separate function)-> unlock that room then, 1st question as well
+
+          await session.commitTransaction();
+          session.endSession();
+        }
+        else if(closeAnswers.has(userAnswerLower))
+        {
+          answerState=1;
+        }
+
+      }   
+    }
+
+    if(answerState==1)
+    {
+      response(res,{solved: "close, close answer"})  
+    }
+    else if(answerState==2)
+    {
+      response(res,{solved: "hurray, correct answer!"})  
+    }
+    else
+    {
+      response(res,{solved: "sorry, incorrect answer!"})
+    }
+
+  }
+  catch(error)
+  {
+    response(res, {}, 400, error.message, false);
+  }
+}
+
+
