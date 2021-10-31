0488c251448c15c411cd9c5bf3e3102a5f4f018a
Sidarth Venkatesan <sidarth.v14002@gmail.com>
2021-10-31 12:53:17 +0530

    app.js fix

--- app.js
+++ app.js
@@ -11,12 +11,9 @@ const authorizedRoutes = require("./routes/authorized");
 const authRoutes = require("./routes/authentication");
 const staticRoutes = require("./routes/static");
 const userRoutes = require("./routes/user");
-<<<<<<< HEAD
 const roomRoutes = require("./routes/room");
-=======
 const transactRoutes = require("./routes/transact");
 const gameRoutes = require("./routes/game");
->>>>>>> 241a6d71617e1b1be820a2317919757efc2334f0
 
 const app = express();
 const DB_URL = process.env.DB_URI;
@@ -36,13 +33,10 @@ app.use("/auth", authRoutes);
 app.use("/authorized", authorized, authorizedRoutes);
 app.use("/static", staticRoutes);
 app.use("/user", authorized, userRoutes);
-<<<<<<< HEAD
 app.use("/room", authorized, roomRoutes);
-=======
 app.use("/transact", authorized, transactRoutes);
 
 app.use("/game", authorized, gameRoutes);
->>>>>>> 241a6d71617e1b1be820a2317919757efc2334f0
 
 app.get("/", (req, res) => {
   res.send("The server is running!");
