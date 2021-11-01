2ab7d131b64ea333384f8c97bbfcb0cf28610ae7
Sidarth Venkatesan <sidarth.v14002@gmail.com>
2021-11-01 17:14:54 +0530

    error checking for current room

--- controllers/storyController.js
+++ controllers/storyController.js
@@ -1,42 +1,50 @@
+const User = require("../models/userModel");
 const Room = require("../models/roomModel");
 const Story = require("../story.json");
 const { response } = require("../config/responseSchema");
 const { getStorySchema } = require("../config/requestSchema");
 
-exports.story = async( req, res ) => {
+exports.story = async (req, res) => {
     try {
+        const id = req.user.id;
+        const Users = await User.find({ "_id": id });
+        const currentRoomId = Users[0].currentRoomId;
         const { roomId } = await getStorySchema.validateAsync(
             req.query
         );
-        const roomData = await Room.find({ "_id": roomId});
+
+        if (roomId != currentRoomId) {
+            throw new Error("Requested story is not from current room");
+        }
+        const roomData = await Room.find({ "_id": roomId });
 
         let data = [];
         Story.forEach(item => {
-            if( item.roomNo == roomData[0].roomNo){
+            if (item.roomNo == roomData[0].roomNo) {
                 data.push(item.message);
             }
         });
         response(res, { story: data });
-    } catch (err){
+    } catch (err) {
         response(res, {}, 400, err.message, false);
     }
 };
 
-exports.fullStory = async( req, res ) => {
+exports.fullStory = async (req, res) => {
     try {
         const { roomId } = await getStorySchema.validateAsync(
             req.query
         );
-        const roomData = await Room.find({ "_id": roomId});
+        const roomData = await Room.find({ "_id": roomId });
 
         let data = [];
         Story.forEach(item => {
-            if( item.roomNo <= roomData[0].roomNo){
+            if (item.roomNo <= roomData[0].roomNo) {
                 data.push(item.message);
             }
         });
         response(res, { story: data });
-    } catch (err){
+    } catch (err) {
         response(res, {}, 400, err.message, false);
     }
 };
