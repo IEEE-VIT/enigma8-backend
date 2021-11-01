0d6ad415b0fd02cda215686af86a41f6fa4072a5
varunmurpani <varunmurpani10@gmail.com>
2021-11-01 14:45:03 +0530

    need more fixes

--- controllers/transactController.js
+++ controllers/transactController.js
@@ -4,6 +4,7 @@ const User = require("../models/userModel");
 const Journey = require("../models/journeyModel");
 const { getQuestionSchema } = require("../config/requestSchema");
 const { response } = require("../config/responseSchema");
+const mongoose = require("mongoose");
 
 exports.getQuestion = async(req,res) => { 
   try {     
@@ -41,3 +42,82 @@ exports.getQuestion = async(req,res) => {
   }
   
 }
+
+exports.submitAnswer= async(req,res) =>{
+  const session = await mongoose.startSession();
+  session.startTransaction();
+  try {
+    const userId= req.user.id;
+    const userAnswer= req.body.userAnswer;
+    const userAnswerLower= userAnswer.toLowerCase();
+    const roomId= req.body.roomId;
+    const currentJourney= await Journey.findOne({roomId,userId})
+
+    let i=0;
+    let answerState=0;
+    for(i=0; i<3;i++){
+      if(currentJourney.questionsStatus[i] === "unlocked")
+      {    
+        const currentRoom= await Room.findOne({_id: roomId})           
+        const questionId= currentRoom.questionId[i]      
+        const currentQuestion= await Question.findOne({_id: questionId})
+        
+        // 0= incorrect ans    1= close answer    2= correct answer
+
+        const correctAnswers= new Set(currentQuestion.answers)
+        const closeAnswers= new Set(currentQuestion.closeAnswers)
+
+        if(correctAnswers.has(userAnswerLower)){
+         
+          //update score using dynamic algo      
+          const addStarAndScore= await User.updateOne(
+            {userId},
+            {$inc:{stars:1}, $inc: {score:100}}, 
+            {session});
+
+          if(!addStarAndScore) throw new Error("Error updating stars and score")
+          answerState= 2;
+
+          const stringg="questionsStatus."+String(i)
+          const query={}
+          query[stringg]="solved"
+          const updateStatus= Journey.updateOne(
+            {userId,roomId},
+            {$set: query},
+            {session});
+          if(!updateStatus) throw new Error("Error updating status of question")    
+
+          //change locked to unlocked next question      
+          //if stars enough to unlock the next room(separate function)-> unlock that room then, 1st question as well
+          
+        }
+        else if(closeAnswers.has(userAnswerLower))
+        {
+          answerState=1;
+        }
+        
+      }   
+    }
+
+    if(answerState==1){
+      response(res,{solved: "close, close answer"})  
+    }
+    else if(answerState==2){
+      response(res,{solved: "hurray, correct answer!"})  
+    }
+    else{
+      response(res,{solved: "sorry, incorrect answer!"})
+    }    
+
+    session.commitTransaction();
+  }
+
+  catch(error){
+    await session.abortTransaction();
+    response(res, {}, 400, error.message, false);
+  }    
+  session.endSession();
+  
+}
+
+
--- routes/transact.js
+++ routes/transact.js
@@ -3,7 +3,9 @@ const router = express.Router();
 const Joi = require("joi");
 
 const {getQuestion} = require('../controllers/transactController');
+const {submitAnswer} = require('../controllers/transactController');
 
 router.get("/getQuestion", getQuestion)
+router.post("/submitAnswer", submitAnswer)
 
 module.exports = router;
