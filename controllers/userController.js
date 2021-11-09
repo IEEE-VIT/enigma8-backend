7ff35af71fd4a247b18d0b0c8b670bde33ebb4f0
Sidarth Venkatesan <64766301+Sidarth-V@users.noreply.github.com>
2021-11-09 16:18:39 +0530

    case insensitive check for unique username #128

--- controllers/userController.js
+++ controllers/userController.js
@@ -28,7 +28,7 @@ exports.createUser = async (req, res) => {
       );
 
     //check if username is available
-    const isUsernameDuplicate = await User.find({ username }).count();
+    const isUsernameDuplicate = await User.find({ 'username': { $regex: new RegExp(`^${username}$`), $options: 'i' } }).count();
     if (isUsernameDuplicate) throw new Error("username not unique");
 
     //write to db
