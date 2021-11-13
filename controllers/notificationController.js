8a2538e5c5a24d23aba9de14b691f7fc8f07ac8c
Anshul Agarwala <anshulagarwala@Anshuls-MacBook-Air.local>
2021-11-13 17:44:30 +0530

    merge conflict

--- controllers/notificationController.js
+++ controllers/notificationController.js
@@ -1,34 +1,12 @@
 const Notification = require("../models/notificatoinModel");
 const { response } = require("../config/responseSchema");
-const mongoose = require("mongoose");
-const logger = require("../config/logger");
-const internal = async (req, res) => {
-  try {
-    var text = req.body.text;
-    var timestamp = req.body.timestamp;
-    var type = req.body.type;
-    var metadata = req.body.metadata;
 
-    const { modifiedCount, matchedCount } = await Notifications.updateOne(
-      { text: text },
-      { timestamp: timestamp },
-      { type: type },
-      { metadata: metadata }
-    );
-  } catch (err) {
-    logger.error(req.user.email + "-> " + err);
-    response(res, {}, 400, err.message, false);
-  }
-};
-
-const notifications = async (req, res) => {
+exports.notifications = async (req, res) => {
   try {
-    const notifs = await Notifications.find().sort({ timestamp: -1 });
+    const notifs = await Notification.find().sort({ timestamp: -1 });
 
     response(res, { notifs });
   } catch (err) {
-    logger.error(req.user.email + "-> " + err);
     response(res, {}, 400, err.message, false);
   }
 };
-module.exports = { notifications };
