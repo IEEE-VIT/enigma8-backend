ead811c043c2b800804bc6d6805d02e43ae153ae
Varun Murpani <82239802+varunmurpani@users.noreply.github.com>
2021-11-16 16:26:44 +0530

    Bugfix varun (#160)

--- controllers/transactController.js
+++ controllers/transactController.js
@@ -30,12 +30,28 @@ exports.getQuestion = async (req, res) => {
     for (let i = 0; i < 3; i++) {
       if (currentJourney.questionsStatus[i] === "unlocked") {
         questionFound = true;
+        const currentPowerupId=currentJourney.powerupId;
+        const powerupDetails= await Powerup.findOne({_id:currentPowerupId});
+        const powerupUsed= currentJourney.powerupUsed;
         const currentRoom = await Room.findOne({ _id: roomId });
         const questionId = currentRoom.questionId[i];
-        const question = await Question.findOne({ _id: questionId }).select(
-          "text media mediaType questionNo currentRoom"
+        let hint= null;
+        const deets = await Question.findOne({ _id: questionId });
+        const question=({_id:deets.id, text:deets.text,media:deets.media,mediaType:deets.mediaType, questionNo:deets.questionNo,currentRoom:deets.currentRoom});
+
+        const currentUserUsedHints = req.user.usedHints.map((id) =>
+          id.toHexString()
         );
-        response(res, question);
+
+        const alreadyUsedHints = new Set(currentUserUsedHints);
+        if (!alreadyUsedHints.has(questionId.toHexString())) {
+          hint=null;
+        }
+        else if(alreadyUsedHints.has(questionId.toHexString())){
+          hint= deets.hint;
+        }
+        
+        response(res, {question,powerupDetails,powerupUsed,hint});
       }
     }
 
