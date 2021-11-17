b102b736cd3c784f0726eebec74efcea281be8bd
Varun Murpani <82239802+varunmurpani@users.noreply.github.com>
2021-11-17 14:56:42 +0530

    removed middleware active from user (#169)

--- routes/user.js
+++ routes/user.js
@@ -1,24 +1,21 @@
 const express = require("express");
 const router = express.Router();
+const isEnigmaActive = require("../middleware/enigmaActive");
 
 const Joi = require("joi");
 
 const {
   createUser,
-  getPowerups,
-  consumePowerup,
+  getPowerups,  
   getUser,
   startJourney,
   addFCM,
 } = require("../controllers/userController");
 
 router.post("/create", createUser);
-
-router.get("/getPowerups", getPowerups);
-
-router.post("/selectPowerup", startJourney);
 router.post("/addFCM", addFCM);
-
 router.get("/getDetails", getUser);
+router.get("/getPowerups", isEnigmaActive, getPowerups);
+router.post("/selectPowerup", isEnigmaActive, startJourney);
 
 module.exports = router;
