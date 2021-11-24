961d08585d47844c813c7ea43cebdc147ca88f06
Anshul Agarwala <ans29hul@gmail.com>
2021-11-24 15:53:35 +0530

    update notif (#207)

---
+++ routes/oracle.js
@@ -0,0 +1,80 @@
+const express = require("express");
+const router = express.Router();
+
+var admin = require("firebase-admin");
+var User = require("../models/userModel");
+var Notification = require("../models/notificationModel");
+
+var serviceAccount = {
+  type: "service_account",
+  project_id: "enigma-8",
+  private_key_id: process.env.ORACLE_PRIV_KEY_ID,
+  private_key: process.env.ORACLE_PRIV_KEY.replace(/\\n/g, "\n"),
+  client_email: process.env.ORACLE_CLIENT_EMAIL,
+  client_id: process.env.ORACLE_CLIENT_ID,
+  auth_uri: "https://accounts.google.com/o/oauth2/auth",
+  token_uri: "https://oauth2.googleapis.com/token",
+  auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
+  client_x509_cert_url: process.env.ORACLE_CERT,
+};
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+});
+const messaging = admin.messaging();
+
+router.post("/all", async (req, res) => {
+  if (req.body.key !== process.env.ORACLE) {
+    res.status(500).send("auth fail");
+    return;
+  }
+
+  if (req.body.type && !req.body.body) {
+    res.status(500).send("Meta data has to be present if type is specified");
+  }
+
+  if (!req.body.delivery) {
+    res.status(500).send("specify delivery mode");
+  }
+
+  if (req.body.delivery === "push|save") {
+    await sendPush(req, res);
+    await saveToDB(req, res);
+  } else if (req.body.delivery === "push") {
+    await sendPush(req, res);
+  } else if (req.body.delivery === "save") {
+    await saveToDB(req, res);
+  }
+});
+
+async function sendPush(req, res) {
+  const user = await User.find({ fcmToken: { $ne: [] } });
+  const token = user.map(({ fcmToken }) => fcmToken);
+  const fcm = [];
+  for (var a in token) {
+    if (token[a]) {
+      fcm.push(...token[a]);
+    }
+  }
+  const registrationTokens = fcm.map(({ token }) => token);
+  const message = {
+    notification: {
+      title: req.body.title,
+      body: req.body.body,
+    },
+    tokens: registrationTokens,
+  };
+
+  messaging.sendMulticast(message).then((response) => {
+    res.status(200).json({ status: "success", response });
+  });
+}
+
+async function saveToDB(req, res) {
+  await new Notification({
+    text: req.body.title,
+    timestamp: new Date(),
+    metadata: req.body.body,
+    type: req.body.type,
+  }).save();
+}
+module.exports = router;
