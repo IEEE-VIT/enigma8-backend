de228999617dec6e2fbffeca5ee0c7718313adca
Anshul Agarwala <anshulagarwala@Anshuls-MacBook-Air.local>
2021-11-25 17:23:11 +0530

    log

---
+++ config/answerLogger.js
@@ -0,0 +1,16 @@
+const { createLogger, format, transports } = require("winston");
+
+module.exports = createLogger({
+  transports: [
+    new transports.File({
+      filename: "logs/answer.log",
+      format: format.combine(
+        format.timestamp({ format: "MMM-DD-YYYY HH:mm:ss" }),
+        format.align(),
+        format.printf(
+          (info) => `${info.level}: ${[info.timestamp]}: ${info.message}`
+        )
+      ),
+    }),
+  ],
+});
