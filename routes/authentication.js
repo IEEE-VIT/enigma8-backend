da85a1f7db0192ab746de2afd20c67035c295742
varunmurpani <varunmurpani10@gmail.com>
2021-11-26 02:36:18 +0530

    fix

--- routes/authentication.js
+++ routes/authentication.js
@@ -43,8 +43,8 @@ router.post("/app/apple", async (req, res) => {
 });
 
 router.post("/web/apple", async (req, res) => {
-  try {
-    const { jwt, isNew } = await verify.appleVerifyWeb(req.body.token);
+  try {    
+    const { jwt, isNew } = await verify.appleVerifyWeb(req.body.id_token);
     response(res, { JWT: jwt, isNew: isNew });
   } catch (err) {
     response(res, {}, 400, JSON.stringify(err), false);
